b3f70c8358e8b388939c054326de9896772f8c6d
anis-zm <anis@zmedia.com.pk>
2026-03-19 08:54:08 +0500

    cors setup

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/shopiclone');
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://anisinbox10_db_user:TPARxjWKVnQJq87U@cluster0.eqaopyk.mongodb.net/?appName=Cluster0');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
